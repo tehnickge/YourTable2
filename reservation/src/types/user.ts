0e2a1802de31f3fd2c00cdb7f5820c73f56173e1
Nikita "Tehnick <71207932+tehnickge@users.noreply.github.com>
2025-02-14 22:36:11 +0300

    add jwt

--- reservation/src/types/user.ts
+++ reservation/src/types/user.ts
@@ -3,12 +3,19 @@ export interface IUser {
   username: string;
   email: string;
   password: string;
-  photo?: Uint8Array;
+  photo?: Uint8Array | null;
+  phoneNumber?: string | null;
   createdAt: Date;
   type: string;
-  historyRests: number[];
+  historyRest: number[];
   wishList: number[];
-  phoneNumber?: string;
+}
+
+export interface IUserPayload {
+  id: number;
+  username: string;
+  email: string;
+  type: string;
 }
 
 export interface IUserValidSchemaRegistration {
